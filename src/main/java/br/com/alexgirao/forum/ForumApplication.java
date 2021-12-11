d86e9db275277dd54ae4ff87cffcd93d017f298d
Alex Girao <fco.alex.girao@gmail.com>
2021-12-10 23:56:27 -0300

    Habilitando cache

--- src/main/java/br/com/alexgirao/forum/ForumApplication.java
+++ src/main/java/br/com/alexgirao/forum/ForumApplication.java
@@ -2,10 +2,14 @@ package br.com.alexgirao.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+// Habilitar ferramentas de Web para Data
 @EnableSpringDataWebSupport
+// habilitando cache
+@EnableCaching
 public class ForumApplication {
 
 	public static void main(String[] args) {
